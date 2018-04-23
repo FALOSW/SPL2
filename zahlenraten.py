76700087c7b5c2e6687030aa8744a46202ac89d3
FALOSW <171wita24@o365.htl-leoben.at>
2018-04-23 16:18:55 +0200

    zahlenraten wurde angelegt

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,4 +1,22 @@
 #zahlenraten.py
 
+import random
+max = 200
+zufallszahl = random.randint(1,max)
+versuche = 0
+gewonnen = False
+print("Bitte geben sie ihre gewünschte Zahl zwischen 1 und", max,"ein")
 
+while(gewonnen == False):
+    zahl = input("Welche zahl raten sie?")
+    zahl = int(zahl)
+    versuche += 1
+    if (zahl == zufallszahl):
+        gewonnen = True
+    if (zahl < zufallszahl):
+        print("Meine gewählte Zahl ist groesser")
+    if (zahl > zufallszahl):
+        print("Meine gewählte Zahl ist kleiner")
+
+print("Bravo! sie die Zahl mit", versuche, "Versuche erraten")
 
