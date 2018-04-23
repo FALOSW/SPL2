b20fa154dda07887b7d328f75e9b67ba1fd534c8
FALOSW <171wita24@o365.htl-leoben.at>
2018-04-23 14:05:32 +0200

    grundlagen wurden erneuert

--- grundlagen-python.py
+++ grundlagen-python.py
@@ -1,1 +1,18 @@
 #Grundlagen-Python.py
+
+#Kommentare erfolgen mit hashtag
+
+#Ausgabe von Daten
+print("Hallo Welt")
+
+#Variable definieren (kann ohne typ erfolgen)
+heimat = "Erde"
+
+#Mehrere Variablen werden mit , getrennt
+print(heimat, "an World: ", "Hallo!")
+
+#Eingabe
+wer = input("Und wer bist du?")
+
+#und gibt den text wieder aus
+print ("Hallo", wer)
